69199ef236eb38f8bbab8dd246f44e5c7add7559
ayasolaiman94 <aya.solaiman@forusinvest.com>
2024-07-26 01:24:43 +0300

    Updating search API | Updating search result rendering

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,39 +1,76 @@
-import { useState, type ChangeEvent } from 'react';
+import { useState, type ChangeEvent } from "react";
 import { getCodeSandboxHost } from "@codesandbox/utils";
 
-type Hotel = { _id: string, chain_name: string; hotel_name: string; city: string, country: string };
+type Hotel = {
+  _id: string;
+  chain_name: string;
+  hotel_name: string;
+  city: string;
+  country: string;
+};
 
-const codeSandboxHost = getCodeSandboxHost(3001)
-const API_URL = codeSandboxHost ? `https://${codeSandboxHost}` : 'http://localhost:3001'
+type Country = {
+  countryisocode: string;
+  country: string;
+};
 
-const fetchAndFilterHotels = async (value: string) => {
-  const hotelsData = await fetch(`${API_URL}/hotels`);
-  const hotels = (await hotelsData.json()) as Hotel[];
-  return hotels.filter(
-    ({ chain_name, hotel_name, city, country }) =>
-      chain_name.toLowerCase().includes(value.toLowerCase()) ||
-      hotel_name.toLowerCase().includes(value.toLowerCase()) ||
-      city.toLowerCase().includes(value.toLowerCase()) ||
-      country.toLowerCase().includes(value.toLowerCase())
-  );
+type City = {
+  name: string;
+};
+
+type Accomodation = {
+  hotels: [],
+  countries: [],
+  cities: []
 }
 
+const codeSandboxHost = getCodeSandboxHost(3001);
+const API_URL = codeSandboxHost
+  ? `https://${codeSandboxHost}`
+  : "http://localhost:3001";
+
+const fetchAccomodations = async (query: string) => {
+  const response = await fetch(`${API_URL}/accomodations`, {
+    method: "POST",
+    body: JSON.stringify({
+      query,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  console.log('Fetched Accomodations:', response);
+  return response.json();
+};
+
 function App() {
-  const [hotels, setHotels] = useState<Hotel[]>([]);
+
+  // set the required local states to render the search result
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [accommodations, setAccomodations] = useState<Accomodation | null>();
+  
   const [showClearBtn, setShowClearBtn] = useState(false);
 
   const fetchData = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value === '') {
-      setHotels([]);
+    if (event.target.value === "") {
+      setSearchQuery('');
       setShowClearBtn(false);
+      setAccomodations(null);
       return;
     }
-
-    const filteredHotels = await fetchAndFilterHotels(event.target.value)
+    
+    setSearchQuery(event.target.value);
+    const filteredAccomodations = await fetchAccomodations(event.target.value);
     setShowClearBtn(true);
-    setHotels(filteredHotels);
+    setAccomodations(filteredAccomodations);
   };
 
+  const clearSearch = () => {
+    setSearchQuery('');
+    setAccomodations(null);
+    setShowClearBtn(false)
+  };
+  
   return (
     <div className="App">
       <div className="container">
@@ -46,30 +83,68 @@ function App() {
                   type="text"
                   className="form-control form-input"
                   placeholder="Search accommodation..."
+                  value={searchQuery}
                   onChange={fetchData}
                 />
                 {showClearBtn && (
-                  <span className="left-pan">
+                  <span className="left-pan" onClick={clearSearch}>
                     <i className="fa fa-close"></i>
                   </span>
                 )}
               </div>
-              {!!hotels.length && (
+              {!!accommodations && (
                 <div className="search-dropdown-menu dropdown-menu w-100 show p-2">
                   <h2>Hotels</h2>
-                  {hotels.length ? hotels.map((hotel, index) => (
-                    <li key={index}>
-                      <a href={`/hotels/${hotel._id}`} className="dropdown-item">
-                        <i className="fa fa-building mr-2"></i>
-                        {hotel.hotel_name}
-                      </a>
-                      <hr className="divider" />
-                    </li>
-                  )) : <p>No hotels matched</p>}
+                  {accommodations?.hotels?.length ? (
+                    accommodations?.hotels.map((hotel: Hotel, index) => (
+                      <li key={index}>
+                        <a
+                          href={`/hotels/${hotel?._id}`}
+                          className="dropdown-item"
+                        >
+                          <i className="fa fa-building mr-2"></i>
+                          {hotel?.hotel_name}
+                        </a>
+                        <hr className="divider" />
+                      </li>
+                    ))
+                  ) : (
+                    <p>No hotels matched</p>
+                  )}
                   <h2>Countries</h2>
-                  <p>No countries matched</p>
+                  {accommodations?.countries?.length ? (
+                    accommodations?.countries.map((country: Country, index) => (
+                      <li key={index}>
+                        <a
+                          href={`/countries/${country?.countryisocode}`}
+                          className="dropdown-item"
+                        >
+                          <i className="fa fa-building mr-2"></i>
+                          {country?.country}
+                        </a>
+                        <hr className="divider" />
+                      </li>
+                    ))
+                  ) : (
+                    <p>No countries matched</p>
+                  )}
                   <h2>Cities</h2>
-                  <p>No cities matched</p>
+                  {accommodations?.cities?.length ? (
+                    accommodations?.cities.map((city: City, index) => (
+                      <li key={index}>
+                        <a
+                          href={`/cities/${city}`}
+                          className="dropdown-item"
+                        >
+                          <i className="fa fa-building mr-2"></i>
+                          {city?.name}
+                        </a>
+                        <hr className="divider" />
+                      </li>
+                    ))
+                  ) : (
+                    <p>No cities matched</p>
+                  )}
                 </div>
               )}
             </div>
